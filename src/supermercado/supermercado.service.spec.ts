13e9f7e2133fc239d83ab3221d6ecfbc6a6a92c0
Harold Funeme <h.funeme@uniandes.edu.co>
2022-09-14 13:22:59 -0500

    :white_check_mark: logic and test supermercado

--- src/supermercado/supermercado.service.spec.ts
+++ src/supermercado/supermercado.service.spec.ts
@@ -1,15 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SupermercadoService } from './supermercado.service';
+import { SupermercadoEntity } from './supermercado.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmTestingConfig } from '../testing-utils/typeorm-testing-config';
 
 describe('SupermercadoService', () => {
   let service: SupermercadoService;
+  let repository: Repository<SupermercadoEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [SupermercadoService],
+      imports: [...TypeOrmTestingConfig()],
     }).compile();
 
     service = module.get<SupermercadoService>(SupermercadoService);
+    repository = module.get<Repository<SupermercadoEntity>>(
+      getRepositoryToken(SupermercadoEntity),
+    );
   });
 
   it('should be defined', () => {
--- src/supermercado/supermercado.service.ts
+++ src/supermercado/supermercado.service.ts
@@ -1,4 +1,82 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  BusinessLogicException,
+  BusinessError,
+} from '../shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { SupermercadoEntity } from './supermercado.entity';
 
 @Injectable()
-export class SupermercadoService {}
+export class SupermercadoService {
+  paisAceptado = ['Argentina', 'Ecuador', 'Paraguay'];
+
+  constructor(
+    @InjectRepository(SupermercadoEntity)
+    private readonly supermercadoEntity: Repository<SupermercadoEntity>,
+  ) {}
+
+  async findAll(): Promise<SupermercadoEntity[]> {
+    return await this.supermercadoEntity.find({ relations: ['supermercado'] });
+  }
+
+  async findOne(id: string): Promise<SupermercadoEntity> {
+    const supermercado: SupermercadoEntity =
+      await this.supermercadoEntity.findOne({
+        where: { id },
+        relations: ['supermercado'],
+      });
+    if (!supermercado)
+      throw new BusinessLogicException(
+        'The supermercado with the given id was not found',
+        BusinessError.NOT_FOUND,
+      );
+
+    return supermercado;
+  }
+
+  async create(supermercado: SupermercadoEntity): Promise<SupermercadoEntity> {
+    if (supermercado.nombre.length > 10)
+      throw new BusinessLogicException(
+        'The supermercado with the given nombre it is not permitted',
+        BusinessError.NOT_FOUND,
+      );
+    return await this.supermercadoEntity.save(supermercado);
+  }
+
+  async update(
+    id: string,
+    supermercado: SupermercadoEntity,
+  ): Promise<SupermercadoEntity> {
+    const persistedsupermercado: SupermercadoEntity =
+      await this.supermercadoEntity.findOne({
+        where: { id },
+      });
+    if (!persistedsupermercado)
+      throw new BusinessLogicException(
+        'The supermercado with the given id was not found',
+        BusinessError.NOT_FOUND,
+      );
+    if (supermercado.nombre.length > 10)
+      throw new BusinessLogicException(
+        'The supermercado with the given nombre it is not permitted',
+        BusinessError.NOT_FOUND,
+      );
+
+    return await this.supermercadoEntity.save(supermercado);
+  }
+
+  async delete(id: string) {
+    const supermercado: SupermercadoEntity =
+      await this.supermercadoEntity.findOne({
+        where: { id },
+      });
+    if (!supermercado)
+      throw new BusinessLogicException(
+        'The supermercado with the given id was not found',
+        BusinessError.NOT_FOUND,
+      );
+
+    await this.supermercadoEntity.remove(supermercado);
+  }
+}
