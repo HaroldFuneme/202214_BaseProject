d9daa62e0534b4c130358ab48209fb38184437bd
Harold Funeme <h.funeme@uniandes.edu.co>
2022-09-14 13:06:15 -0500

    :white_check_mark: fix test of ciudad

--- src/ciudad/ciudad.service.spec.ts
+++ src/ciudad/ciudad.service.spec.ts
@@ -1,15 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CiudadService } from './ciudad.service';
+import { CiudadEntity } from './ciudad.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmTestingConfig } from '../testing-utils/typeorm-testing-config';
 
 describe('CiudadService', () => {
   let service: CiudadService;
+  let repository: Repository<CiudadEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CiudadService],
+      imports: [...TypeOrmTestingConfig()],
     }).compile();
 
     service = module.get<CiudadService>(CiudadService);
+    repository = module.get<Repository<CiudadEntity>>(
+      getRepositoryToken(CiudadEntity),
+    );
   });
 
   it('should be defined', () => {
