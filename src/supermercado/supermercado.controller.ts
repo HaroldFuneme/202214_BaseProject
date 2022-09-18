9cf86fcc244c0d7727b5b99fda0bfd809511bc8d
Harold Funeme <h.funeme@uniandes.edu.co>
2022-09-18 09:35:20 -0500

    :truck: add controller supermercado

--- src/supermercado/supermercado.controller.ts
+++ src/supermercado/supermercado.controller.ts
@@ -1,6 +1,59 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { SupermercadoDto } from './supermercado.dto';
+import { SupermercadoEntity } from './supermercado.entity';
+import { SupermercadoService } from './supermercado.service';
 
 @Controller('/supermarkets')
 @UseInterceptors(BusinessErrorsInterceptor)
-export class SupermercadoController {}
+export class SupermercadoController {
+  constructor(private readonly supermercadoService: SupermercadoService) {}
+
+  @Get()
+  async findAll() {
+    return await this.supermercadoService.findAll();
+  }
+
+  @Get(':supermercadoId')
+  async findOne(@Param('supermercadoId') supermercadoId: string) {
+    return await this.supermercadoService.findOne(supermercadoId);
+  }
+
+  @Post()
+  async create(@Body() SupermercadoDto: SupermercadoEntity) {
+    const museum: SupermercadoEntity = plainToInstance(
+      SupermercadoEntity,
+      SupermercadoDto,
+    );
+    return await this.supermercadoService.create(museum);
+  }
+
+  @Put(':supermercadoId')
+  async update(
+    @Param('supermercadoId') supermercadoId: string,
+    @Body() SupermercadoDto: SupermercadoDto,
+  ) {
+    const supermercado: SupermercadoEntity = plainToInstance(
+      SupermercadoEntity,
+      SupermercadoDto,
+    );
+    return await this.supermercadoService.update(supermercadoId, supermercado);
+  }
+
+  @Delete(':supermercadoId')
+  @HttpCode(204)
+  async delete(@Param('supermercadoId') supermercadoId: string) {
+    return await this.supermercadoService.delete(supermercadoId);
+  }
+}
